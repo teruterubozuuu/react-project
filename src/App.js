c3d0247bb884c4fa0f1143fe7038049e0932c99d
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-28 09:26:26 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HI GURL</h1>
+      <h2>MEOW MEOW MEOW MEOW</h2>
     </div>
   );
 }
